b5d48f333c565e1f1ecba29035bac07bc2facc00
pcpc466 <prashantch.94@gmail.com>
2023-02-15 09:27:54 -0700

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Prashant Singh Chauhan on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
